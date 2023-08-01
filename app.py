c24e5f3a71bf9321a1cb66dda586b152a17fc2a4
farahalysa <89579571+farahalysa@users.noreply.github.com>
2023-08-01 07:39:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,25 @@ app = Flask(__name__)
 def recomendation_obat(brand):
     drug = pickle.load(open('drug.pkl','rb'))
     similarity_matrix = pickle.load(open('similarity_matrix.pkl','rb'))
+    threshold=0.35
     indexprod = drug[drug['brand'] == brand].index[0]
     similar_review = list(enumerate(similarity_matrix[indexprod]))
-    sorted_similar_review = sorted(similar_review, key=lambda x:x[1], reverse=True)
+    filtered_sim_scores = [score for score in similar_review if score[1] > threshold]
+    sorted_similar_review = sorted(filtered_sim_scores, key=lambda x:x[1], reverse=True)
     x = []
-    for i in range(1,8) :
-        x.append(sorted_similar_review[i][0])
-    return drug.iloc[x,:13]
+    for i in range(1, min(15, len(sorted_similar_review))):
+        indx = sorted_similar_review[i][0]
+        x.append([
+            drug.iloc[indx, 0],
+            drug.iloc[indx, 1],
+            drug.iloc[indx, 2],
+            drug.iloc[indx, 3],
+            drug.iloc[indx, 4],
+            drug.iloc[indx, 5]
+        ])
+    df = pd.DataFrame(x, columns=["Brand", "Contents", "Indications/Uses", "Dosage/Direction for Use", 
+                                              "Contraindications", "Adverse Reactions"])
+    return df
 
 
 @app.route("/")
@@ -37,7 +49,7 @@ def recommendation():
         drug = pickle.load(open('drug.pkl','rb'))
         indices = pd.Series(drug.index, index=drug['brand']).drop_duplicates()
         idx = indices[request.form["drugs"]]
-        the_drugs = list(enumerate(drug.iloc[idx,:13], 1))
+        the_drugs = list(enumerate(drug.iloc[idx,:6], 1))
 
         for _, row in df.iterrows():
             rows.append(list(enumerate(row, 1)))
